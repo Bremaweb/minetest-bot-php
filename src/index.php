640d7ccdfa026fdd1af102310f6ce5613b5926c1
Robert Arkenin <rarkenin@gmail.com>
2013-02-06 18:19:30 -0500

    Added some files

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+define("CMDTOSERVER", 1);
+define("CMDSENDRELIABLE", 2);
+define("CMDACKNEEDED", 4);
+define("CMDACTIONRELIABLE",8);
+class MTBotcommand {
+
+    public static $byte;
+    public static $direction;
+    public static $ackCommand;
+
+}
+
+?>
