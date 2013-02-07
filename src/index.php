00f08efbbf99189585b431ab914562928e8ea6c6
Robert Arkenin <rarkenin@gmail.com>
2013-02-06 20:15:23 -0500

    Giant restructuring

--- src/index.php
+++ src/index.php
@@ -1,15 +1,21 @@
 <?php
 
-define("CMDTOSERVER", 1);
+define("CMDTOSERVER", 1); //Otherwise to client
 define("CMDSENDRELIABLE", 2);
 define("CMDACKNEEDED", 4);
-define("CMDACTIONRELIABLE",8);
-class MTBotcommand {
-
-    public static $byte;
-    public static $direction;
-    public static $ackCommand;
-    public static $unpackString;
+define("CMDACTIONRELIABLE", 8);
+if (PHP_SAPI != "cli") {
+    die("Main [FATAL]:Must be run in CLI mode!");
+}
+if ($argc != 3) {
+    die("Main [FATAL]:Improper arguments!\r\n            Usage: minetestbot.php host port controlfile");
+}
+$serverIp = $argv[1];
+$serverPort = $argv[2];
+$controlFile = $argv[3];
+$serverSocket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+if (false == (@socket_connect($serverSocket, $serverIp, $serverPort))) {
+   echo "Connect [FATAL]:Connect failed: reason: " . socket_strerror(socket_last_error($serverSocket)) . "\n";
 }
 
 ?>
